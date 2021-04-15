3286816fc9d560292d0c498ca1b341c3ed4718c7
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-04-15 13:55:34 +0530

    java steup in vs code

---
+++ hello world/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("hello world... i am running java program in vs code");
+    }
+}
--- object1/build/classes/object1/Object1.rs
+++
@@ -1,5 +0,0 @@
-object1.Object1
-object1.can
-object1.learn
-object1.any
-object1.body
